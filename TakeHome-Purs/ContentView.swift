d9e893e528a44a9f2673335dde11c58d5887c905
anagha-kj <159282427+anagha-kj@users.noreply.github.com>
2024-05-20 13:19:01 -0700

    Initial Commit

---
+++ TakeHome-Purs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TakeHome-Purs
+//
+//  Created by Anagha K J on 20/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TakeHome-Purs/TakeHome_PursApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHome_PursApp.swift
+//  TakeHome-Purs
+//
+//  Created by Anagha K J on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TakeHome_PursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
